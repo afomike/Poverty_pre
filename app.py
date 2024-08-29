7c0b196b7c6071e668119316a086b10d68d8fedd
afomike <temidayoafote@gmail.com>
2024-08-29 20:55:40 +0100

    new

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import pickle
 import joblib
 
 app = Flask(__name__)
 
-#  Load the pre-trained model and encoders
-models = joblib.load('models/models.pkl')
-label_encoders = joblib.load('models/label_encoders.pkl')
-scaler = joblib.load('models/scaler.pkl')
+# Load the pre-trained model and encoders
+model = joblib.load('models/random_forest.pkl')  # Load a single model
+label_encoders = joblib.load('models/label_encoders.pkl')  # Load encoders as a dictionary
 
 
 def poverty_level(income):
-    if income < 15000:
+    if income == 1:
         return 'Below Poverty Line'
-    elif 15000 <= income < 50000:
+    elif income == 2:
         return 'Middle Class'
-    else:
+    elif income == 0:
         return 'Above Poverty Line'
+    else:
+        return 'unconcluded'
 
 @app.route('/')
 def index():
@@ -38,18 +40,24 @@ def predict():
                 known_classes = set(le.classes_)
                 df[column] = df[column].apply(lambda x: le.transform([x])[0] if x in known_classes else -1)
 
-    # Scale features
-    df[['Age', 'Income']] = df[['Age', 'Income']].astype(float)
-    df[['Age', 'Income']] = scaler.transform(df[['Age', 'Income']])
+    
 
-    # Predict with one model (e.g., the first model in the dictionary)
-    selected_model = list(models.values())[1]  # Choose the desired model
-    prediction = selected_model.predict(df)[0]
+    # Predict using the loaded model
+    prediction = model.predict(df)[0]
 
+    # Ensure the prediction is an integer for the poverty_level function
+    prediction = int(prediction)
+    
     # Map prediction to human-readable label
     outcome = poverty_level(prediction)
 
     return jsonify({'result': outcome})
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
